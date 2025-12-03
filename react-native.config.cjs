ce53139a3075edf47e8fbedd81bf96b2bda1b3d5
Olivier Louvignes <olivier@mgcrea.io>
2025-12-03 22:29:35 +0100

    fix: configure codegen to use build directory
    - Remove includesGeneratedCode option to let Gradle run codegen
    - Update paths to use build/generated/source/codegen
    - Apply react plugin unconditionally for proper codegen setup
    - Remove placeholder JNI files (generated by codegen)
    - Add react-native.config.cjs to package files

--- react-native.config.cjs
+++ react-native.config.cjs
@@ -2,14 +2,10 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: './android',
-        packageImportPath:
-          'import com.mgcrea.reactnative.jetpackcompose.RNJetpackComposePackage;',
-        packageInstance: 'new RNJetpackComposePackage()',
-        componentDescriptors: [
-          'ColorViewComponentDescriptor',
-        ],
-        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
+        sourceDir: "./android",
+        packageImportPath: "import com.mgcrea.reactnative.jetpackcompose.RNJetpackComposePackage;",
+        packageInstance: "new RNJetpackComposePackage()",
+        cmakeListsPath: "src/main/jni/CMakeLists.txt",
       },
     },
   },
